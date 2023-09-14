59f84c2da428e2a2e019228bfcc0fa9def186388
Luciano Petillo <luciano.prnl@gmail.com>
2023-09-15 00:50:19 +0200

    feat: new station model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_174206) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_224208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_174206) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_stations_on_company_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,5 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_174206) do
   end
 
   add_foreign_key "companies", "users"
+  add_foreign_key "stations", "companies"
   add_foreign_key "users", "roles"
 end
