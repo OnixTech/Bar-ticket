3f50bf7aa9f7536d9395e08bf0cd036c1a26fa98
Luciano Petillo <luciano.prnl@gmail.com>
2023-09-14 15:19:43 +0200

    feat: companies controller added

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
+  resources :companies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
