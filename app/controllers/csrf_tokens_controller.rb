54f6d7763286d44d393d3ef54b4f77a8c7d5321a
Luciano Petillo <luciano.prnl@gmail.com>
2023-10-03 22:12:17 +0200

    fix: csrfTokens deleted

--- app/controllers/csrf_tokens_controller.rb
+++
@@ -1,8 +0,0 @@
-class CsrfTokensController < ApplicationController
-    skip_before_action :authenticate_user!, only: :show
-    def show
-        skip_authorization
-        csrf_token = form_authenticity_token
-        render json: { csrf_token: csrf_token }
-    end
-end
