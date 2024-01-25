f7e20aa0a04cd570880600a44d44fd6fa8ae789c
Luciano Petillo <luciano.prnl@gmail.com>
2024-01-25 13:29:52 +0100

    fix: assets application file. Style: margins on stn-show

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,5 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "./controllers/websocket_order_controller"
+import "bootstrap"
